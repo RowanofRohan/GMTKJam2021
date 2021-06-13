afdf5be34fd4c7b2275246b85956550b702476d7
aurinredgrave <eylul.atilgan@gmail.com>
2021-06-13 19:26:34 +0300

    Spawn enemies, vfxs, damage stuff.

--- Assets/FireArea.cs
+++ Assets/FireArea.cs
@@ -5,16 +5,33 @@ using UnityEngine;
 public class FireArea : MonoBehaviour
 {
     public GameObject fireParticleGO;
+    public GameObject bigExplosionParticle;
     public int radius;
+    GameObject explosion;
+    List<GameObject> fireParticles;
 
+    private void Awake()
+    {
+        fireParticles = new List<GameObject>();
+    }
     void OnEnable()
     {
-        for (int i = 0; i < 10; i++)
+        explosion = Instantiate(bigExplosionParticle.gameObject, transform.position, Quaternion.identity);
+        Invoke("BurnGround", 1f);
+    }
+
+    private void BurnGround()
+    {
+        Destroy(explosion);
+        for (int i = 0; i < 20; i++)
         {
             var pos = Random.insideUnitCircle * radius;
             GameObject fire = Instantiate(fireParticleGO, transform, false);
             fire.transform.localPosition = new Vector2(pos.x, pos.y);
+            fireParticles.Add(fire);
         }
+
+        Invoke("PutOffFire", 5f);
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
@@ -24,5 +41,15 @@ public class FireArea : MonoBehaviour
         {
             hit.Burn(1000);
         }
+    }
+
+    public void PutOffFire()
+    {
+        foreach (var item in fireParticles)
+        {
+            item.GetComponent<ParticleSystem>().Stop();
+        }
+
+        Destroy(gameObject, 2f);
     }
 }
