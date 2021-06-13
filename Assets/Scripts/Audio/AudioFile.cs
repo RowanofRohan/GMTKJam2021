0c5a9910679f0d704b123b25640bb7b82ce5ad0a
orumcan <orumcan@gmail.com>
2021-06-13 19:33:44 +0300

    Add Audio Manager Add BGM Tweak enemy prefab

---
+++ Assets/Scripts/Audio/AudioFile.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class AudioFile
+{
+    public string audioName;
+    public AudioClip audioClip;    
+    [Range(0f, 1f)]
+    public float volume;
+    public bool isMute;
+    [HideInInspector]
+    public AudioSource source;
+    public bool isLooping;
+    public bool playOnAwake;
+}
